fbb3708c9baeb2b5cc83ac47ec16f209911ee57a
ghedarav <ghedarav@gmail.com>
2019-06-02 19:54:10 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,9 @@
 <html>
 <head>
+  
 <title> My PHP APP - DEMO </title>
 <body>
-
+<h1> mon equipe  demo </h1>
 </br> php version is <?php echo $PHP_VERSION ; ?>
 </br> Server IP  <?php echo $_SERVER['SERVER_ADDR']; ?>
 
