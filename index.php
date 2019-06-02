9b61bb47eedf46b883d77f6f4cc9ec77e4ea5557
ghedarav <ghedarav@gmail.com>
2019-06-02 19:40:38 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+<title> My PHP APP - DEMO </title>
+<body>
+
+</br> php version is <?php echo $PHP_VERSION ; ?>
+</br> Server IP  <?php echo $_SERVER['SERVER_ADDR']; ?>
+
+</body>
+</head>
+</html>
